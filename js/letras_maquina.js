a1cd833bf88831404c314c639867736b7e32c8ab
Adrian Ruiz <adrian@werblix.com>
2020-10-27 11:14:56 -0300

    cambios en css

---
+++ js/letras_maquina.js
@@ -0,0 +1,34 @@
+// Función texto maquina escribir hero Home
+
+var words = ['Planifica...', 'Elabora...', 'Procesa...', 'Gestiona...', 'Administra...', 'Factura...', 'Registra...', 'Agiliza...', 'Contabiliza...'],
+    wordWrapper = document.getElementById('word'),
+    wordWrapperContent = wordWrapper.innerHTML,
+    addingWord = false,
+    counter = 1;
+
+setInterval(function(){
+
+    if(wordWrapperContent.length > 0 && !addingWord ) {
+        wordWrapper.innerHTML = wordWrapperContent.slice(0, -1);
+        wordWrapperContent = wordWrapper.innerHTML;
+    } else {
+        addingWord = true;
+    }
+
+    if( addingWord ){
+        if( wordWrapperContent.length < words[counter].length  ) {
+        wordWrapper.innerHTML = words[counter].slice(0, wordWrapperContent.length + 1);
+        wordWrapperContent = wordWrapper.innerHTML;
+        } else {
+        if( counter < words.length) {
+            counter ++
+        }
+        addingWord = false;
+        }
+    }
+
+    if( counter == words.length) {
+        counter = 0;
+    }
+
+}, 200);
