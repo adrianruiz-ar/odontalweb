d3b2fad7f3b4af024c26619eb8b39cb1f8547429
Adrian Ruiz <adrian@werblix.com>
2021-01-18 14:19:39 -0300

    cambios en tipos de usuarios y footer

---
+++ js/efectos.js
@@ -0,0 +1,29 @@
+const checkbox = document.getElementById('switch-label');
+
+checkbox.addEventListener( 'change', function() {
+
+    let antiguoImg = document.getElementById('user_antiguo_img');
+    let antiguoTexto = document.getElementById('user_antiguo_texto');
+
+    let modernoImg = document.getElementById('user_moderno_img');
+    let modernoTexto = document.getElementById('user_moderno_texto');
+
+
+    if(this.checked) {
+
+        antiguoImg.classList.add("user_ocultar");
+        antiguoTexto.classList.add("user_ocultar");
+
+        modernoImg.classList.remove("user_ocultar");
+        modernoTexto.classList.remove("user_ocultar");
+    
+    }
+    else {
+        
+        antiguoImg.classList.remove("user_ocultar");
+        antiguoTexto.classList.remove("user_ocultar");
+
+        modernoImg.classList.add("user_ocultar");
+        modernoTexto.classList.add("user_ocultar");
+    }
+});
